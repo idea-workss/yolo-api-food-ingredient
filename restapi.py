4aeac75dadbf9d379c966e0043d042b9091694f4
Sekigahara <androaprila12@gmail.com>
2021-08-23 14:34:49 +0700

    Update Brightness and Intefere

--- restapi.py
+++ restapi.py
@@ -32,11 +32,17 @@ def predict():
         image_file = request.files["image"]
         image_bytes = image_file.read()
 
-        #img = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), 1)
-        #img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # Load with opencv
+        img = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), 1)
+
+        # Convert to PIL format
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(img)
+        img = np.asarray(img)
+
         #brightness = np.ones(img.shape, dtype="uint8") * 10
         #img = cv2.add(img, brightness)
-        img = Image.open(io.BytesIO(image_bytes))
+        #img = Image.open(io.BytesIO(image_bytes))
 
         model.conf = 0.1
         results = model(img, size=312)
